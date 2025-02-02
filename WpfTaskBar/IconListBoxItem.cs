3901ca1b12663ef8a96f6ae1efeffc5ac8c2fecc
y-mayo-dev <y.mayo.dev@gmail.com>
2025-02-02 12:40:34 +0900

    ListBoxを追加してタスク一覧を表示できるようにしました。

---
+++ WpfTaskBar/IconListBoxItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace WinFormsTaskBar
+{
+	public class IconListBoxItem
+	{
+		public IconListBoxItem(string text, Image icon, IntPtr handle)
+		{
+			Text = text;
+			Icon = icon;
+			Handle = handle;
+		}
+
+		public string Text { get; set; }
+		public Image? Icon { get; set; }
+		public IntPtr Handle { get; set; }
+
+		public override string ToString()
+		{
+			return Text;
+		}
+	}
+}
--- WpfTaskBar/MainWindow.xaml.cs
+++ WpfTaskBar/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -8,6 +7,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WinFormsTaskBar;
+using Window = System.Windows.Window;
 
 namespace WpfApp1;
 
@@ -16,9 +17,72 @@ namespace WpfApp1;
 /// </summary>
 public partial class MainWindow : Window
 {
+	private WindowManager windowManager = new WindowManager();
+	
 	public MainWindow()
 	{
 		Console.OutputEncoding = Encoding.UTF8;
 		InitializeComponent();
+		
+		windowManager.Start();
+		
+		windowManager.WindowListChanged += WindowManagerOnWindowListChanged;
+	}
+
+	private void WindowManagerOnWindowListChanged(object sender, TaskBarWindowEventArgs e)
+	{
+		Dispatcher.Invoke(() =>
+		{
+			if (e.AddedWindows.Count > 0)
+			{
+				foreach (var window in e.AddedWindows)
+				{
+					var iconListBoxItem = new IconListBoxItem(window.Title, window.IconFilePath != null ? GetIcon(window.IconFilePath) : null, window.Handle);
+					listBox.Items.Add(iconListBoxItem);
+				}
+			}
+
+			object[] items = new object[listBox.Items.Count];
+			listBox.Items.CopyTo(items, 0);
+
+			foreach (var handle in e.RemovedWindowHandles)
+			{
+				for (int i = items.Length - 1; i >= 0; --i)
+				{
+					var item = items[i];
+					if (item is IconListBoxItem window)
+					{
+						if (window.Handle == handle)
+						{
+							listBox.Items.RemoveAt(i);
+						}
+					}
+				}
+			}
+		});
+	}
+
+	private void MainWindow_OnClosed(object? sender, EventArgs e)
+	{
+		windowManager.Stop();
+	}
+	
+	public static System.Drawing.Image? GetIcon(string iconFilePath)
+	{
+		try
+		{
+			System.Drawing.Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(iconFilePath);
+			System.Drawing.Image image = icon.ToBitmap();
+			icon.Dispose();
+			return image;
+		}
+		catch (System.ComponentModel.Win32Exception ex)
+		{
+			if (ex.Message.Contains("アクセスが拒否されました"))
+			{
+				return null;
+			}
+			throw;
+		}
 	}
 }
