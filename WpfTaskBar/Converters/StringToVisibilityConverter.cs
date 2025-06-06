e2681707eb17f995bbc215cb9e365e1c10397883
y-mayo-dev <y.mayo.dev@gmail.com>
2025-06-06 23:08:40 +0900

    StringToVisibilityConverterのConvertBackを実装

--- WpfTaskBar/Converters/StringToVisibilityConverter.cs
+++ WpfTaskBar/Converters/StringToVisibilityConverter.cs
@@ -7,13 +7,17 @@ namespace WpfTaskBar.Converters;
 
 public class StringToVisibilityConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         return string.IsNullOrEmpty(value as string) ? Visibility.Collapsed : Visibility.Visible;
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        if (value is Visibility visibility)
+        {
+            return visibility == Visibility.Visible ? string.Empty : null;
+        }
+        return null;
     }
 } 
