a1565626f36e5a7bf11e9ef9c895ba84a9ccef65
y-mayo-dev <y.mayo.dev@gmail.com>
2025-11-14 23:56:15 +0900

    WebViewのnpm run devが動作しなくなっていたため修正しました。

--- WebView/vite.config.ts
+++ WebView/vite.config.ts
@@ -1,30 +1,50 @@
-import { defineConfig, Plugin } from 'vite'
+import {defineConfig, Plugin, UserConfig} from 'vite'
+
 
 function removeModuleType(): Plugin {
-  return {
-    name: 'remove-module-type',
-    transformIndexHtml(html) {
-      return html.replace(/\s*crossorigin/g, '').replace(/\s*type="module"/g, '')
+    return {
+        name: 'remove-module-type',
+        transformIndexHtml(html) {
+            return html.replace(/\s*crossorigin/g, '').replace(/\s*type="module"/g, '')
+        }
     }
-  }
 }
 
-export default defineConfig({
-  base: './',
-  plugins: [removeModuleType()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      },
-      output: {
-        format: 'iife',
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
+let config: UserConfig;
+
+if (process.env.NODE_ENV === 'production') {
+    config = {
+        base: './',
+        plugins: [removeModuleType()],
+        build: {
+            outDir: 'dist',
+            emptyOutDir: true,
+            rollupOptions: {
+                input: {
+                    main: './index.html'
+                },
+                output: {
+                    format: 'iife',
+                    entryFileNames: 'assets/[name]-[hash].js',
+                    chunkFileNames: 'assets/[name]-[hash].js',
+                    assetFileNames: 'assets/[name]-[hash].[ext]'
+                }
+            }
+        }
     }
-  }
-})
+} else {
+    config = {
+        base: './',
+        build: {
+            outDir: '../dist',
+            emptyOutDir: true,
+            rollupOptions: {
+                input: {
+                    main: './index.html'
+                }
+            }
+        }
+    }
+}
+
+export default defineConfig(config)
